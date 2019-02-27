24046441c6291d1294907d77b9609ff88958a52d
heweizong <heweizong@itouchtv.cn>
2019-02-27 12:19:25 +0800

    [新增]hook传感器参数

--- app/src/main/java/com/dovar/fakermobile/XposedUtil.java
+++ app/src/main/java/com/dovar/fakermobile/XposedUtil.java
@@ -13,6 +13,7 @@ import com.dovar.fakermobile.util.DataUtil;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -260,5 +261,36 @@ public class XposedUtil implements IXposedHookLoadPackage {
         }
 
         return false;
+    }
+
+    private int DURATION_INTERVAL = 10 * 1000;//限制模拟摇一摇最小时间间隔，即连续两次摇一摇的最小时间间隔
+    private long lastUpdateTime; // 上次检测时间
+    /**
+     * hook传感器
+     * 模拟摇一摇或用于修改步数
+     */
+    private void hookSensor(XC_LoadPackage.LoadPackageParam loadPackageParam) {
+        final Class<?> sensorEL = XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue", loadPackageParam.classLoader);
+        XposedBridge.hookAllMethods(sensorEL, "dispatchSensorEvent", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                ((float[]) param.args[1])[0] = (125.0F + 1200.0F * new Random().nextFloat());
+                XposedBridge.log("dispatchSensorEvent");
+
+                long currentUpdateTime = System.currentTimeMillis();
+                long timeInterval = currentUpdateTime - lastUpdateTime;
+                if (timeInterval < DURATION_INTERVAL) {
+                    //小于最小时间间隔，return
+                    super.beforeHookedMethod(param);
+                    return;
+                }
+                ((float[]) param.args[1])[0] = (10.0F + 1000.0F * new Random().nextFloat());
+                ((float[]) param.args[1])[1] = (10.0F + 1000.0F * new Random().nextFloat());
+                ((float[]) param.args[1])[2] = (10.0F + 1000.0F * new Random().nextFloat());
+                //更新lastUpdateTime
+                lastUpdateTime = currentUpdateTime;
+                super.beforeHookedMethod(param);
+            }
+        });
     }
 }
